ab590d9e316a3ec1cf0f6ebca49c4a3f1c2507ec
CoderChang <cheng.chang@sjtu.edu.cn>
2016-03-15 13:29:52 +0800

    rename hello.py to server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
-    print filename
+    #print filename
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
